c4a1adf8b51aa96eb99571b07f1803b20af032c3
DonKimeu <57639381+DonKimeu@users.noreply.github.com>
2020-05-26 00:30:42 +0300

    minor changes

--- county_mix.php
+++ county_mix.php
@@ -1,7 +1,5 @@
 <?php
-/**
-  Counties in the Republic of Kenya,Governors and their Political Parties.
-*/
+/**Counties in the Republic of Kenya,Governors and their Political Parties.*/
 
 $county_data = "1. Mombasa. – Hassan Ali Joho – ODM,
 2. Kwale. – Salim Mvurya – Jubilee,
@@ -51,10 +49,183 @@ $county_data = "1. Mombasa. – Hassan Ali Joho – ODM,
 46. Nyamira. – John Nyangarama Obiena - ODM,
 47. Nairobi. – Mike Sonko Mbuvi Kioko – Jubilee";
 
-/**
- You should not modify anything above this line
-*/
+/**You should not modify anything above this line*/
 
 // @TODO : Your code starts here
-
+$dividedCountyData = explode(",",$county_data);
+$arrlength = count($dividedCountyData);
+$nonrequired = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9","."," ' ");
+$jub=0;
+$odm=0;
+$kan=0;
+$ford=0;
+$ccm=0;
+$wdm=0;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Assignment 1</title>
+  
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css">
+  <style>
+
+background
+{
+	color:gold;
+}
+	
+    table, th, td {
+		
+		    
+      border:0.2px solid black;
+    }
+	
+  </style>
+</head>
+
+	
+	
+	
+<body>
+<div class="has-background-white">
+<div class="container is-fluid">
+  <table style="width:100%" class="
+table is-striped">
+  <div class="is-block">
+  <div class="notification">
+  <thead>
+    <tr class="has-background-primary">
+      <th>#	</th>
+      <th>Name</th>
+      <th>Governor</th>
+      <th>Website</th>
+    </tr>
+    </thead>
+    <tbody>
+	
+
+    <?php 
+    $n=1; 
+    for($j = 0; $j < $arrlength; $j++) {?>
+      <?php $splitStringData = explode(" – ",$dividedCountyData[$j]);
+      $datalength = count($splitStringData);?>
+      <tr>
+      <td><?php echo $n++?></td>  
+      <td>
+	  
+	  
+
+      <?php 
+          $modifieDstring = str_replace($nonrequired, "", $splitStringData[0]);
+          echo $modifieDstring;?>
+      </td>
+	  
+	  
+      <td>
+      <?php 
+          if(!empty($splitStringData[2])){ 
+            echo $splitStringData[1];
+			
+			if($splitStringData[2]=="ODM"){
+					$odm++;
+			?>
+			
+            <img width="5%" height="20%" src="images/odm.png">
+			<?php }
+			
+			if($splitStringData[2]=="Jubilee"){
+				$jub++;
+			?>
+			
+            <img width="5%" height="20%" src="images/JUBILEE.png">
+			<?php }
+			
+			if($splitStringData[2]=="CCM"){
+				$ccm++;
+			?>
+			
+            <img width="5%" height="20%" src="images/JUBILEE.png">
+			<?php }
+			
+			if($splitStringData[2]=="KANU"){
+				
+				$kan++;
+			?>
+			
+            <img width="5%" height="20%" src="images/KANU.png">
+			<?php }
+			
+		    if($splitStringData[2]=="FORD Kenya"){
+					$ford++;
+			?>
+			
+            <img width="5%" height="20%" src="images/FORD.jpg">
+			<?php }
+			
+			if($splitStringData[2]=="WDM K"){
+				$wdm++;
+			?>
+			
+            <img width="5%" height="20%" src="images/WDM.png">
+			<?php }
+			if($splitStringData[1]=="Mandera"){
+				echo"Ali Ibrahim Roba";
+			?>
+			
+             
+			<?php }
+			
+			if($splitStringData[1]=="Nyamira"){
+				echo"John Nyangarama Obiena";
+			?>
+			
+            <img width="5%" height="20%" src="images/WDM.png">
+			<?php }
+			
+			}
+        else {
+           
+        }
+        ?>
+      </td>
+      <?php 
+       $modifieDstring = str_replace($nonrequired, "", $splitStringData[0]);
+       $nospacedlink = strtolower(str_replace(' ', '', $modifieDstring));?>
+       <td><a target="_BLANK" href="http://<?php echo $nospacedlink?>.go.ke"><?php echo "http://".$nospacedlink.".go.ke";?></a></td>
+    </tr>
+	<div class="$footer-padding">
+    <?php 
+      
+
+	  }
+
+
+							    
+						
+    ?>
+	</div>
+    </tbody>
+  </table>
+<footer class="footer">
+  <div class="content has-text-centered">
+  <?php
+  		    echo "The number of governors in jubilee is ".$jub ."   <br> ";
+			    echo "The number of governors in ODM is ".$odm."   <br> ";
+				    echo "The number of governors in Ford Kenya is ".$ford."   <br> ";
+					    echo "The number of governors in KANU is ".$kan."   <br> ";
+						   echo "The number of governors in CCM is ".$ccm."   <br> ";
+						      echo "The number of governors in WDM is ".$wdm."   <br> ";
+  ?>
+    </div>
+	</footer>
+  </div>
+    </div>
+	  </div>
+	   </div>
+</body>
+
+</html>
