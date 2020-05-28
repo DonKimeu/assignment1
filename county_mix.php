80ab2a0df7d7c695e5cc4037a47bb2e3181b555b
DonKimeu <57639381+DonKimeu@users.noreply.github.com>
2020-05-28 22:35:37 +0300

    Add files via upload

--- county_mix.php
+++ county_mix.php
@@ -9,7 +9,7 @@ $county_data = "1. Mombasa. – Hassan Ali Joho – ODM,
 6. Taita-Taveta. – Granton Samboja – WDM K,
 7. Garissa. – Ali Bunow Korane – Jubilee,
 8. Wajir. – Mohamed Abdi Mohamud – Jubilee,
-9. Mandera. – Ali Ibrahim Roba - Jubilee,
+9. Mandera. – Ali Ibrahim Roba – Jubilee,
 10. Marsabit – Mohamud Mohamed Ali – Jubilee,
 11. Isiolo – Mohamed Abdi Kuti – Independent,
 12. Meru – Kiraitu Murungi – Jubilee,
@@ -46,21 +46,26 @@ $county_data = "1. Mombasa. – Hassan Ali Joho – ODM,
 43. Homabay. – Cyprian Awiti – ODM,
 44. Migori. – Zachary Okoth Obado – ODM,
 45. Kisii. – James Ongware – ODM,
-46. Nyamira. – John Nyangarama Obiena - ODM,
+46. Nyamira. – John Nyangarama Obiena – ODM,
 47. Nairobi. – Mike Sonko Mbuvi Kioko – Jubilee";
 
 /**You should not modify anything above this line*/
 
 // @TODO : Your code starts here
 $dividedCountyData = explode(",",$county_data);
+
 $arrlength = count($dividedCountyData);
-$nonrequired = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9","."," ' ");
+$nonrequired = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",".");
+
 $jub=0;
 $odm=0;
 $kan=0;
 $ford=0;
 $ccm=0;
 $wdm=0;
+$ind=0;
+$nar=0;
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -73,14 +78,9 @@ $wdm=0;
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css">
   <style>
 
-background
-{
-	color:gold;
-}
 	
     table, th, td {
-		
-		    
+
       border:0.2px solid black;
     }
 	
@@ -93,8 +93,8 @@ background
 <body>
 <div class="has-background-white">
 <div class="container is-fluid">
-  <table style="width:100%" class="
-table is-striped">
+  <table style="width:100%" class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth
+">
   <div class="is-block">
   <div class="notification">
   <thead>
@@ -109,32 +109,45 @@ table is-striped">
 	
 
     <?php 
+	
+	
     $n=1; 
     for($j = 0; $j < $arrlength; $j++) {?>
       <?php $splitStringData = explode(" – ",$dividedCountyData[$j]);
       $datalength = count($splitStringData);?>
       <tr>
-      <td><?php echo $n++?></td>  
-      <td>
-	  
-	  
+      <td><?php echo $n++?></td>  <!--  column for county numbers-->
+	  <!-- column for the county names -->
 
-      <?php 
-          $modifieDstring = str_replace($nonrequired, "", $splitStringData[0]);
+      <td> 
+	  
+      <?php
+	  
+          $modifieDstring = str_replace($nonrequired,"", $splitStringData[0]);
           echo $modifieDstring;?>
+		  
       </td>
-	  
-	  
-      <td>
+
+	  <!-- for the county Governors           	 echo $splitStringData[2];   -->
+      <td> 
       <?php 
+	  
           if(!empty($splitStringData[2])){ 
+		  
             echo $splitStringData[1];
-			
+		
 			if($splitStringData[2]=="ODM"){
 					$odm++;
 			?>
 			
             <img width="5%" height="20%" src="images/odm.png">
+			<?php }
+			
+			if($splitStringData[2]=="Independent"){
+					$ind++;
+			?>
+			
+            <img width="5%" height="20%" src="images/INDEPENDENT.png">
 			<?php }
 			
 			if($splitStringData[2]=="Jubilee"){
@@ -143,12 +156,21 @@ table is-striped">
 			
             <img width="5%" height="20%" src="images/JUBILEE.png">
 			<?php }
+
+
+						if($splitStringData[2]=="Narc Kenya"){
+				$nar++;
+			?>
+			
+            <img width="5%" height="20%" src="images/NARC.jpeg">
+			<?php }
+			
 			
 			if($splitStringData[2]=="CCM"){
 				$ccm++;
 			?>
 			
-            <img width="5%" height="20%" src="images/JUBILEE.png">
+            <img width="5%" height="20%" src="images/CCM.jpg">
 			<?php }
 			
 			if($splitStringData[2]=="KANU"){
@@ -170,26 +192,20 @@ table is-striped">
 				$wdm++;
 			?>
 			
+
+			
             <img width="5%" height="20%" src="images/WDM.png">
-			<?php }
-			if($splitStringData[1]=="Mandera"){
-				echo"Ali Ibrahim Roba";
-			?>
+			<?php } 
 			
-             
-			<?php }
-			
-			if($splitStringData[1]=="Nyamira"){
-				echo"John Nyangarama Obiena";
+    if($splitStringData[2]=="WDM K."){
+				$wdm++;
 			?>
 			
             <img width="5%" height="20%" src="images/WDM.png">
 			<?php }
 			
-			}
-        else {
-           
-        }
+		  }
+
         ?>
       </td>
       <?php 
@@ -197,7 +213,7 @@ table is-striped">
        $nospacedlink = strtolower(str_replace(' ', '', $modifieDstring));?>
        <td><a target="_BLANK" href="http://<?php echo $nospacedlink?>.go.ke"><?php echo "http://".$nospacedlink.".go.ke";?></a></td>
     </tr>
-	<div class="$footer-padding">
+
     <?php 
       
 
@@ -210,15 +226,19 @@ table is-striped">
 	</div>
     </tbody>
   </table>
+  	<div class="$footer-padding">
 <footer class="footer">
   <div class="content has-text-centered">
   <?php
-  		    echo "The number of governors in jubilee is ".$jub ."   <br> ";
-			    echo "The number of governors in ODM is ".$odm."   <br> ";
-				    echo "The number of governors in Ford Kenya is ".$ford."   <br> ";
-					    echo "The number of governors in KANU is ".$kan."   <br> ";
-						   echo "The number of governors in CCM is ".$ccm."   <br> ";
-						      echo "The number of governors in WDM is ".$wdm."   <br> ";
+  		    echo "     The number of governors in jubilee is 	:	".$jub ."   <br> ";
+			    echo "     The number of governors in ODM is 	:	".$odm."   <br> ";
+				    echo "         The number of governors in Ford Kenya is	:	 ".$ford."   <br> ";
+					    echo "   The number of governors in KANU is 	:	".$kan."   <br> ";
+						   echo "The number of governors in CCM is 	:	".$ccm."   <br> ";
+						      echo "  The number of governors in WDM is 	:	".$wdm."   <br> ";
+							echo "   The number of governors in NARC kenya is	:	 ".$nar."   <br> ";
+							  	      echo "   The number of Independent governors is 	:	".$ind."   <br> ";
+									  
   ?>
     </div>
 	</footer>
